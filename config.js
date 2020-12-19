2c9138e31f5c710f3b276d6ecf450161e880b28f
adriendt22 <55588625+adriendt22@users.noreply.github.com>
2020-12-19 18:34:02 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-exports.TOKEN = "Nzg4ODU4MjY4ODgxMzg3NTUy.X9pngw.z3djetOA03gRDfx6NlqzVbcf45A";
 exports.PREFIX = "$";
 exports.FOOTER = "Créer par $AdriXn DT#9999";
 exports.FOOTERI = "https://i.imgur.com/DZpPUem.png";
