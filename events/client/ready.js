359d6d890f471a8208fcdc4dc6adff83ad90617c
adriendt22 <55588625+adriendt22@users.noreply.github.com>
2021-01-04 21:09:01 +0100

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,5 +1,10 @@
 module.exports = client => {
   console.log(`Connecté à ${client.user.tag}!`);
 
- client.user.setStatus('dnd');
+ client.user.setPresence({
+  activity:{
+    name: 'WSH LES GENS'
+  },
+  status: 'dnd'
+});
 }
